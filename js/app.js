5541a2fa5377d9a675a4c22057617ce44bdfe2b5
Abhiek187 <achaudhuri2011@yahoo.com>
2018-05-29 16:05:39 -0400

    feat: Initialize game engine

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+    this.y = 0;
+    this.speed = 0;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    for (const enemy of allEnemies)
+        enemy.x = enemy.speed * dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,17 +29,49 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
+    constructor() {
+        this.sprite = 'images/char-boy.png';
+        this.x = 0;
+        this.y = 0;
+    }
 
+    update() {
+
+    }
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    handleInput(dir) {
+        switch (dir) {
+            case 'left':
+                this.x -= 50;
+                break;
+            case 'up':
+                this.y -= 50;
+                break;
+            case 'right':
+                this.x += 50;
+                break;
+            case 'down':
+                this.y += 50;
+                break;
+        }
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+let allEnemies = [new Enemy(), new Enemy(), new Enemy()];
+let player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+// Note: 'keydown' used instead 'keyup' for immediate player movement
+document.addEventListener('keydown', function(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',

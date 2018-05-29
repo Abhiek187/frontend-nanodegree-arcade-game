3f45c2da5ba303e8ff2170882a7c348699073c43
Abhiek187 <achaudhuri2011@yahoo.com>
2018-05-29 19:07:04 -0400

    style: Add comments and make Enemy a class

--- js/app.js
+++ js/app.js
@@ -1,54 +1,57 @@
 // Enemies our player must avoid
-const Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+class Enemy {
+    constructor() {
+        // Variables applied to each of our instances go here,
+        // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    this.reset();
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    for (const enemy of allEnemies) {
-        enemy.x += enemy.speed * dt;
-
-        // Respawn enemy if out of bounds
-        if (enemy.x >= 503)
-            enemy.reset();
-
-        // Detect enemy collision
-        if (Math.abs(enemy.x - player.x) <= 67 && enemy.y === player.y)
-            player.reset();
+        // The image/sprite for our enemies, this uses
+        // a helper we've provided to easily load images
+        this.sprite = 'images/enemy-bug.png';
+        this.reset();
     }
-};
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+        // You should multiply any movement by the dt parameter
+        // which will ensure the game runs at the same speed for
+        // all computers.
+        for (const enemy of allEnemies) {
+            enemy.x += enemy.speed * dt;
 
-Enemy.prototype.reset = function() {
-    this.x = -103;
-    this.y = (() => {
-        const row = Math.floor(Math.random() * 3); // y position is an integer
+            // Respawn enemy if out of bounds
+            if (enemy.x >= 503)
+                enemy.reset();
 
-        switch(row) {
-            case 0:
-                return 68;
-            case 1:
-                return 151;
-            case 2:
-                return 234;
+            // Detect enemy collision
+            if (Math.abs(enemy.x - player.x) <= 67 && enemy.y === player.y)
+                player.reset();
         }
-    })(); // execute function immediately and assign this.y to return value
-    this.speed = Math.random() * 100 + 50; // speed is a double
-};
+    }
+
+    // Draw the enemy on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    // Reset the enemy speed and position
+    reset() {
+        this.x = -103;
+        this.y = (() => {
+            const row = Math.floor(Math.random() * 3); // y position is an integer
+
+            switch(row) {
+                case 0:
+                    return 68;
+                case 1:
+                    return 151;
+                case 2:
+                    return 234;
+            }
+        })(); // execute function immediately and assign this.y to return value
+        this.speed = Math.random() * 100 + 50; // speed is a double
+    }
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -59,12 +62,15 @@ class Player {
         this.reset();
     }
 
-    update() {}
+    update() {} // unused
 
+    // Draw the player on screen
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 
+    // Move player based on input
+    // Parameter: dir, the player's direction
     handleInput(dir) {
         switch (dir) {
             // Note: px increases right and down
@@ -86,11 +92,11 @@ class Player {
                 break;
         }
 
-        if (this.y === -15) { // -15px = water (player won)
+        if (this.y === -15) // -15px = water (player won)
             this.reset();
-        }
     }
 
+    // Reset player position
     reset() {
         this.x = 200;
         this.y = 400; // bottom center of canvas
@@ -100,13 +106,13 @@ class Player {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-let allEnemies = [new Enemy(), new Enemy(), new Enemy()];
-let player = new Player();
+const allEnemies = [new Enemy(), new Enemy(), new Enemy()];
+const player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-// Note: 'keydown' used instead 'keyup' for immediate player movement
-document.addEventListener('keydown', function(e) {
+// Note: 'keydown' used instead 'keyup' for immediate button response
+document.addEventListener('keydown', e => {
     const allowedKeys = {
         37: 'left',
         38: 'up',
